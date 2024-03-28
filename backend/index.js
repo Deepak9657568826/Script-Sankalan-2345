5d887d17784c27c45c3e9c35393d0c2bad7d6a9c
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-03-28 22:59:23 +0530

    userModel and installed some dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.port;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('This is home routes');
+    res.send('This is home routes');
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`);
 });
