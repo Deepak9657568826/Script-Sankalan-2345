86befdedbf4358eca08acce8355fc5fac61eb514
Raushan8505 <raushanrajsingh46@gmail.com>
2024-03-30 18:13:35 +0530

    Backend models added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    pass: { type: String, required: true },
+    
+    points: { type: Number },
+    solved_questions: [{ type: String }],
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-})
-
-const UserModel = mongoose.model('user', UserSchema);
+const UserModel = mongoose.model("users", userSchema);
 
 module.exports = {
-    UserModel
-}
+  UserModel,
+};
